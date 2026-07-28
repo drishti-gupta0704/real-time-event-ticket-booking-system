f1e35a621bc4a3f6d478c670e956179208e062a2
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-07-28 21:16:44 +0530

    configured mongodb and generated jwt secret key

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 
 const express = require("express");
-
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+dotenv.config();
+connectDB();
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.send("Event Ticket Booking API is running");
 });
 
-app.listen(4000, () => {
-    console.log("Server running");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });

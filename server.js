38f9bede192da95b39b4afb132534eca6c66b877
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-08-01 14:02:50 +0530

    added routes to main server file

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Event Ticket Booking API is running");

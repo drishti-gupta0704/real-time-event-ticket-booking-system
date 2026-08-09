680bcc8a42083d563e6af79f3a499097708875c6
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-08-09 18:43:58 +0530

    created event

---
+++ controllers/eventController.js
@@ -0,0 +1,87 @@
+const Event = require("../models/Event");
+
+const createEvent = async (req, res) => {
+
+    try {
+
+        const {
+            title,
+            description,
+            venue,
+            city,
+            date,
+            time,
+            category,
+            price,
+            totalSeats
+        } = req.body;
+
+        if (
+            !title ||
+            !description ||
+            !venue ||
+            !city ||
+            !date ||
+            !time ||
+            !category ||
+            price === undefined ||
+            totalSeats === undefined
+        ) 
+        
+        {
+            return res.status(400).json({
+                message: "All fields are required"
+            });
+        }
+
+
+        if (price < 0) {
+            return res.status(400).json({
+                message: "Price cannot be negative"
+            });
+        }
+
+
+        if (totalSeats <= 0) {
+            return res.status(400).json({
+                message: "Total seats must be greater than 0"
+            });
+        }
+
+
+        const event = await Event.create({
+            title,
+            description,
+            venue,
+            city,
+            date,
+            time,
+            category,
+            price,
+            totalSeats,
+            availableSeats: totalSeats,
+            createdBy: req.user._id
+        });
+
+
+        res.status(201).json({
+            success: true,
+            message: "Event created successfully",
+            event
+        });
+
+    } 
+    
+    catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+};
+
+
+module.exports = {
+    createEvent
+};

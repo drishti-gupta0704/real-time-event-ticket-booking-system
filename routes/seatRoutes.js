63c4f40f3f1d559b6c5c16185f2d8e994fd3a17b
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-08-12 18:24:36 +0530

    added routes for function to fetch seat details via id

--- routes/seatRoutes.js
+++ routes/seatRoutes.js
@@ -1,12 +1,13 @@
 
 const express = require("express");
 
-const { generateSeats,getEventSeats } = require("../controllers/seatController");
+const { generateSeats,getEventSeats,getSeatById } = require("../controllers/seatController");
 const authMiddleware = require("../middleware/authMiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
 
 const router = express.Router();
 router.post( "/:eventId/generate", authMiddleware, adminMiddleware, generateSeats);
 router.get( "/event/:eventId",getEventSeats);
+router.get("/:seatId",getSeatById );
 
 module.exports = router;

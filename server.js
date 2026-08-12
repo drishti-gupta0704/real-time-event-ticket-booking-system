64db17b4666a8398e011e90d722fe981b5de1714
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-08-12 18:08:59 +0530

    updating the seat routes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const eventRoutes = require("./routes/eventRoutes");
+const seatRoutes = require("./routes/seatRoutes");
+
 dotenv.config();
 connectDB();
 const app = express();
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
+app.use("/api/seats", seatRoutes);
 
 app.get("/", (req, res) => {
     res.send("Event Ticket Booking API is running");

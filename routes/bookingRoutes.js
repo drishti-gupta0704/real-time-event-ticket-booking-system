87da0ca9ca04596c2568b1a1717a47d9d12087b1
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-08-25 11:41:52 +0530

    adding booking routes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { createBooking} = require("../controllers/bookingController");
+const { createBooking , getMyBookings, getBookingById} = require("../controllers/bookingController");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post( "/:eventId", authMiddleware, createBooking );
+router.get( "/my-bookings", authMiddleware, getMyBookings );
+router.get( "/:bookingId", authMiddleware, getBookingById );
+
 
 
 module.exports = router;

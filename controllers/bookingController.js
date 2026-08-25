4db273881ded92233adb0c86da2d3ba4ff5ed91c
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-08-25 18:04:15 +0530

    creating admin management system where he can see everyone booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -224,6 +224,32 @@ const getBookingById = async (req, res) => {
 
 };
 
+const getAllBookings = async (req, res) => {
+    try {
+        const bookings = await Booking.find()
+            .populate("user", "name email")
+            .populate("event", "title venue city date time price")
+            .populate("seats", "seatNumber status")
+            .sort({ createdAt: -1 });
+
+        res.status(200).json({
+            success: true,
+            count: bookings.length,
+            bookings
+        });
+
+    } 
+    
+    catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+};
+
+
+
 const cancelBooking = async (req, res) => {
 
     try {
@@ -252,15 +278,15 @@ const cancelBooking = async (req, res) => {
         }
 
 
-        if (booking.status !== "confirmed") {
-            return res.status(400).json({
-                message: "Only confirmed bookings can be cancelled"
-            });
+         if ( booking.status !== "confirmed" && booking.status !== "pending") {
+               return res.status(400).json({
+               message: "Booking cannot be cancelled"
+              });
         }
 
       
 
-        
+
         if (booking.paymentStatus === "paid") {
              if (!booking.razorpayPaymentId) {
                 return res.status(400).json({
@@ -343,5 +369,6 @@ module.exports = {
     createBooking,
     getMyBookings,
     getBookingById,
+    getAllBookings,
     cancelBooking
 };
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const { createBooking , getMyBookings, getBookingById, cancelBooking} = require("../controllers/bookingController");
+const { createBooking , getMyBookings, getBookingById, getAllBookings, cancelBooking} = require("../controllers/bookingController");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post( "/:eventId", authMiddleware, createBooking );
 router.get( "/my-bookings", authMiddleware, getMyBookings );
 router.get( "/:bookingId", authMiddleware, getBookingById );
+router.get("/", authMiddleware, getAllBookings);
 router.delete( "/:bookingId", authMiddleware, cancelBooking );
+
 
 
 

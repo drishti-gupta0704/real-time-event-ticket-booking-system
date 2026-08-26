72397cb647167987b8e56a8e32579879b7661975
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-08-26 13:52:51 +0530

    integrating Socket.IO and email routes into server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io");
-
 const cors = require("cors");
 const dotenv = require("dotenv");
+
 dotenv.config();
+
 const connectDB = require("./config/db");
 const { connectRedis } = require("./config/redis");
+const { initializeSocket } = require("./config/socket");
+
 const authRoutes = require("./routes/authRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 const seatRoutes = require("./routes/seatRoutes");
@@ -18,35 +20,15 @@ const emailRoutes = require("./routes/emailRoutes");
 
 const app = express();
 
-
-
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-});
-
-
-io.on("connection", (socket) => {
-
-    console.log("Socket connected:", socket.id);
-
-    socket.on("disconnect", () => {
-        console.log("Socket disconnected:", socket.id);
-    });
-
-});
-
-
+initializeSocket(server);
 
 connectDB();
 
-
 app.use(express.json());
 connectRedis();
-
 app.use(cors());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/seats", seatRoutes);
@@ -60,10 +42,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

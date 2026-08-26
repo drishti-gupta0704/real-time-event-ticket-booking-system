ccc23bdad15461d89c44b1dec5d8fbc48b712432
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-08-26 13:48:49 +0530

    configured socket io for real time updates

---
+++ config/socket.js
@@ -0,0 +1,39 @@
+
+let io;
+
+const initializeSocket = (server) => {
+
+    const { Server } = require("socket.io");
+
+    io = new Server(server, {
+        cors: {
+            origin: "*"
+        }
+    });
+
+    io.on("connection", (socket) => {
+
+        console.log("Socket connected:", socket.id);
+
+        socket.on("disconnect", () => {
+            console.log("Socket disconnected:", socket.id);
+        });
+
+    });
+
+    return io;
+};
+
+const getIO = () => {
+
+    if (!io) {
+        throw new Error("Socket.IO has not been initialized");
+    }
+
+    return io;
+};
+
+module.exports = {
+    initializeSocket,
+    getIO
+};
